d535cc3e435143cc815b7dca3f44835d27159389
Ashwini2591 <ashwini.r2591@gmail.com>
2020-08-14 09:26:58 +0530

    firstallfilecommit

---
+++ src/test/java/GitTest/GitTest.java
@@ -0,0 +1,20 @@
+package GitTest;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
+
+public class GitTest {
+
+	
+	@Test
+	public void logintest(){
+		WebDriver wd=new FirefoxDriver();
+		wd.get("http://www.google.com");
+		System.out.println(wd.getTitle());
+		wd.findElement(By.name("q")).sendKeys("chennai");
+		wd.findElement(By.name("q")).submit();
+	}
+
+}
